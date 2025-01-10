d1039687198f64f99ed2a745f231fc493877fd93
JonasPfi <jonas.pfingstag@gmx.de>
2025-01-10 23:39:59 +0100

    high consumption task & low

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -18,6 +18,28 @@ void process(char *array)
 	}
 }
 
+void process_high_power(char *array)
+{
+    while (1) {
+        for (int i = 0; i < 5; i++) {
+            // Intensivere Berechnungen oder Anweisungen für höheren Stromverbrauch
+            for (volatile int j = 0; j < 100000; j++);  // Dummy loop for additional load
+            uart_writeByteBlockingActual(array[i]);
+            delay(50000);  // Kürzeres Delay, um häufiger aktiv zu sein
+        }
+    }
+}
+
+void process_low_power(char *array)
+{
+    while (1) {
+        for (int i = 0; i < 5; i++) {
+            uart_writeByteBlockingActual(array[i]);
+            delay(200000);  // Längeres Delay für weniger aktive Perioden
+        }
+    }
+}
+
 
 void main () {
     uart_init();
@@ -38,7 +60,7 @@ void main () {
     printf("Current instructions counted: %d\n\r", current_count);
     reset_instruction_count();
 
-	int res = copy_process((unsigned long)&process, (unsigned long)"12345");
+	int res = copy_process((unsigned long)&process_low_power, (unsigned long)"12345");
 	if (res != 0) {
 		printf("error while starting process 1");
 		return;
@@ -52,7 +74,7 @@ void main () {
     printf("Current instructions counted: %d\n\r", current_count);
     reset_instruction_count();
 
-	res = copy_process((unsigned long)&process, (unsigned long)"abcde");
+	res = copy_process((unsigned long)&process_high_power, (unsigned long)"abcde");
 	if (res != 0) {
 		printf("error while starting process 2");
 		return;
